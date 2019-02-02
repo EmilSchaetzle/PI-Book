2590445052874a811e4f2d593781a59931aadbdf
Emil Schätzle <mail@emilschaetzle.de>
2019-02-02 16:56:39 +0100

    Changed Oscillator to 8 MHz, set fuses to correct values

--- keyboard/avr_program/fuse.c
+++ keyboard/avr_program/fuse.c
@@ -1,7 +1,8 @@
 #include <avr/io.h>
-
+// Set Fuses with "avrdude -v -p m2560 -c usbasp -U efuse:w:0xFD:m"
+// Upload to board with "avrdude -C"$(PROJECTDIR)avrdude.conf" -v -p m2560 -c usbasp -P/dev/ttyACM0 -D -Uflash:w:"$(PROJECTDIR)bin/Release/avr_program.hex":i"
 FUSES = {
-    .low = LFUSE_DEFAULT,
-    .high = HFUSE_DEFAULT,
-    .extended = EFUSE_DEFAULT
+    .low = 0x5C,
+    .high = 0xD9,
+    .extended = 0xFF
 };
