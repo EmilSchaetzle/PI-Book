3f344c46bad7188d3d75a5cb3e61ef342c808276
Emil Schätzle <mail@emilschaetzle.de>
2019-02-04 22:49:42 +0100

    Updated commands

--- keyboard/avr_program/fuse.c
+++ keyboard/avr_program/fuse.c
@@ -1,6 +1,6 @@
 #include <avr/io.h>
-// Set Fuses with "avrdude -v -p m2560 -c usbasp -U efuse:w:0xFD:m"
-// Upload to board with "avrdude -C"$(PROJECTDIR)avrdude.conf" -v -p m2560 -c usbasp -P/dev/ttyACM0 -D -Uflash:w:"$(PROJECTDIR)bin/Release/avr_program.hex":i"
+// Set Fuses with "avrdude -C"$(PROJECTDIR)avrdude.conf" -v -p m2560 -c avrisp -P /dev/ttyACM0 -b 19200  -U efuse:w:0xFD:m"
+// Upload to board with "avrdude -C"$(PROJECTDIR)avrdude.conf" -v -p m2560 -c avrisp -P /dev/ttyACM0 -b 19200 -Uflash:w:"$(PROJECTDIR)bin/Release/avr_program.hex":i"
 FUSES = {
     .low = 0x5C,
     .high = 0xD9,
