18745fbc1514e10b3dd091d81eb948540e93ca40
Emil Schätzle <mail@emilschaetzle.de>
2019-02-07 22:12:31 +0100

    Changed implementation of pin mapping

--- keyboard/pibook-hid-driver-0.01/pibook-hid-driver.c
+++ keyboard/pibook-hid-driver-0.01/pibook-hid-driver.c
@@ -28,6 +28,9 @@ static int mouse_factor = 15;
 module_param(mouse_factor, int, 0644);
 MODULE_PARM_DESC(mouse_factor, "Factor to scale the mouse movement.");
 
+// List of keys in correct order
+const int pin_to_key[] = {KEY_ESC, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12, KEY_DELETE, KEY_GRAVE, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0, KEY_MINUS, KEY_EQUAL, KEY_BACKSPACE, KEY_TAB, KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T, KEY_Y, KEY_U, KEY_I, KEY_O, KEY_P, KEY_LEFTBRACE, KEY_RIGHTBRACE, KEY_BACKSLASH, KEY_CAPSLOCK, KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_K, KEY_L, KEY_SEMICOLON, KEY_APOSTROPHE, KEY_ENTER, KEY_LEFTSHIFT, KEY_Z, KEY_X, KEY_C, KEY_V, KEY_B, KEY_N, KEY_M, KEY_COMMA, KEY_DOT, KEY_SLASH, KEY_RIGHTSHIFT, KEY_LEFTCTRL, KEY_102ND, KEY_LEFTMETA, KEY_LEFTALT, KEY_SPACE, KEY_RIGHTALT, KEY_COMPOSE, KEY_LEFT, KEY_UP, KEY_DOWN, KEY_RIGHT};
+
 int GPIO_IRQ;
 ktime_t last;
 bool receiving = false;
@@ -73,165 +76,12 @@ static int get_BIT_DURs(void)
 // Converts a received pin value to an LINUX Key Value
 static int map_pin(int pin)
 {
-    switch (pin)
+    if (pin >= 0 && pin <= PIN_MAX)
     {
-    case 0:
-        return KEY_ESC;
-    case 1:
-        return KEY_F1;
-    case 2:
-        return KEY_F2;
-    case 3:
-        return KEY_F3;
-    case 4:
-        return KEY_F4;
-    case 5:
-        return KEY_F5;
-    case 6:
-        return KEY_F6;
-    case 7:
-        return KEY_F7;
-    case 8:
-        return KEY_F8;
-    case 9:
-        return KEY_F9;
-    case 10:
-        return KEY_F10;
-    case 11:
-        return KEY_F11;
-    case 12:
-        return KEY_F12;
-    case 13:
-        return KEY_DELETE;
-    case 14:
-        return KEY_GRAVE;
-    case 15:
-        return KEY_1;
-    case 16:
-        return KEY_2;
-    case 17:
-        return KEY_3;
-    case 18:
-        return KEY_4;
-    case 19:
-        return KEY_5;
-    case 20:
-        return KEY_6;
-    case 21:
-        return KEY_7;
-    case 22:
-        return KEY_8;
-    case 23:
-        return KEY_9;
-    case 24:
-        return KEY_0;
-    case 25:
-        return KEY_MINUS;
-    case 26:
-        return KEY_EQUAL;
-    case 27:
-        return KEY_BACKSPACE;
-    case 28:
-        return KEY_TAB;
-    case 29:
-        return KEY_Q;
-    case 30:
-        return KEY_W;
-    case 31:
-        return KEY_E;
-    case 32:
-        return KEY_R;
-    case 33:
-        return KEY_T;
-    case 34:
-        return KEY_Y;
-    case 35:
-        return KEY_U;
-    case 36:
-        return KEY_I;
-    case 37:
-        return KEY_O;
-    case 38:
-        return KEY_P;
-    case 39:
-        return KEY_LEFTBRACE;
-    case 40:
-        return KEY_RIGHTBRACE;
-    case 41:
-        return KEY_BACKSLASH;
-    case 42:
-        return KEY_CAPSLOCK;
-    case 43:
-        return KEY_A;
-    case 44:
-        return KEY_S;
-    case 45:
-        return KEY_D;
-    case 46:
-        return KEY_F;
-    case 47:
-        return KEY_G;
-    case 48:
-        return KEY_H;
-    case 49:
-        return KEY_J;
-    case 50:
-        return KEY_K;
-    case 51:
-        return KEY_L;
-    case 52:
-        return KEY_SEMICOLON;
-    case 53:
-        return KEY_APOSTROPHE;
-    case 54:
-        return KEY_ENTER;
-    case 55:
-        return KEY_LEFTSHIFT;
-    case 56:
-        return KEY_Z;
-    case 57:
-        return KEY_X;
-    case 58:
-        return KEY_C;
-    case 59:
-        return KEY_V;
-    case 60:
-        return KEY_B;
-    case 61:
-        return KEY_N;
-    case 62:
-        return KEY_M;
-    case 63:
-        return KEY_COMMA;
-    case 64:
-        return KEY_DOT;
-    case 65:
-        return KEY_SLASH;
-    case 66:
-        return KEY_RIGHTSHIFT;
-    case 67:
-        return KEY_LEFTCTRL;
-    case 68:
-        return KEY_102ND;
-    case 69:
-        return KEY_LEFTMETA;
-    case 70:
-        return KEY_LEFTALT;
-    case 71:
-        return KEY_SPACE;
-    case 72:
-        return KEY_RIGHTALT;
-    case 73:
-        return KEY_COMPOSE;
-    case 74:
-        return KEY_LEFT;
-    case 75:
-        return KEY_UP;
-    case 76:
-        return KEY_DOWN;
-    case 77:
-        return KEY_RIGHT;
-    default:
+        return pin_to_key[pin];
+    }
+    else
+    {
         return -1;
     }
 }
