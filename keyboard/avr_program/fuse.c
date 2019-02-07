61d15c87ce04501973b09d7ada0c90bf54ccc74a
Emil Schätzle <mail@emilschaetzle.de>
2019-02-07 17:58:11 +0100

    Changed Format

--- keyboard/avr_program/fuse.c
+++ keyboard/avr_program/fuse.c
@@ -1,7 +1,8 @@
 #include <avr/io.h>
 // Set Fuses with "avrdude -C"$(PROJECTDIR)avrdude.conf" -v -p m2560 -c avrisp -P /dev/ttyACM0 -b 19200  -U efuse:w:0xFD:m"
 // Upload to board with "avrdude -C"$(PROJECTDIR)avrdude.conf" -v -p m2560 -c avrisp -P /dev/ttyACM0 -b 19200 -Uflash:w:"$(PROJECTDIR)bin/Release/avr_program.hex":i"
-FUSES = {
+FUSES =
+{
     .low = 0x5C,
     .high = 0xD9,
     .extended = 0xFF
